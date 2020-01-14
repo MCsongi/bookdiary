5754a3b03f289b21ab06d7fae1e4e254220c62b9
MCsongi <milik.csongor@gmail.com>
2020-01-14 19:07:48 +0200

    Sql files and EER diagram fine tuning

--- src/main/resources/static/mysql/book_details.sql
+++ src/main/resources/static/mysql/book_details.sql
@@ -8,12 +8,10 @@ CREATE TABLE `book_details` (
   `read_times` int(2) unsigned DEFAULT NULL,
   `book_id` bigint(10) unsigned DEFAULT NULL,
   `user_id` bigint(10) unsigned DEFAULT NULL,
-  `quote_id` bigint(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`book_details_id`),
   UNIQUE KEY `book_details_id_UNIQUE` (`book_details_id`),
   KEY `book_details_book_fk_idx` (`book_id`),
   KEY `book_details_user_fk_idx` (`user_id`),
-  KEY `book_details_quote_fk_idx` (`quote_id`),
   CONSTRAINT `book_details_book_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON UPDATE CASCADE,
   CONSTRAINT `book_details_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
--- src/main/resources/static/mysql/quote.sql
+++ src/main/resources/static/mysql/quote.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `quote` (
   `quote_id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `quote_body` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `book_details_id` bigint(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`quote_id`),
   UNIQUE KEY `quote_id_UNIQUE` (`quote_id`),
-  CONSTRAINT `quote_book_details_fk` FOREIGN KEY (`quote_id`) REFERENCES `book_details` (`quote_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `quote_book_details_fk_idx` (`book_details_id`),
+  CONSTRAINT `quote_book_details_fk` FOREIGN KEY (`book_details_id`) REFERENCES `book_details` (`book_details_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
